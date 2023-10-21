db3c687bf91da0d14b104eebfc94f886429dbbbe
Nicolas Garcia Santelices <ngarcia1621@gmail.com>
2023-10-21 19:23:57 -0300

    Updated shadow in home

--- components/ui/MainCard.js
+++ components/ui/MainCard.js
@@ -13,7 +13,7 @@ export default function MainTittleCard (props) {
         <Card
             isFooterBlurred
             radius="lg"
-            className="col-span-12 sm:col-auto  h-full  sm:h-[40rem] shadow-2xl items items-center hover:scale-110 hover:shadow-lg duration-300"
+            className="col-span-12 sm:col-auto  h-full  sm:h-[40rem] shadow-xl items items-center hover:scale-110 hover:shadow-2xl duration-300"
             isPressable onPress = {() => (disabled || route === '/reports') ? null : router.push(route)}
         >
             <CardHeader className="absolute z-10 top-1 flex-col drop-shadow-6xl !items-start ">
