f5e4029df757d89aadbd655737423fab044a4e64
Nicolas Garcia Santelices <ngarcia1621@gmail.com>
2023-11-25 18:00:32 -0300

    added loading in payment button

--- components/ui/PaymentButton.js
+++ components/ui/PaymentButton.js
@@ -1,16 +1,26 @@
 /* eslint-disable no-unused-vars */
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '@nextui-org/react'
 import useSalesStore from '@/app/(layout-app)/sales/store'
 
 export default function PaymentButton ({ key, id, icon, title, paymentTarget, setPaymentTarget, voucherTarget, setVoucherTarget }) {
+    const [loadingPaymentIntegration, setLoadingPaymentIntegration] = useState(false)
+    const [isSelect, setIsSelect] = useState(false)
+    const timeout = setTimeout(function () {
+        console.log('Hello from setTimeout')
+    }, 5000)
     const handleButton = () => {
         if (setPaymentTarget) {
-            setPaymentTarget(
+            setLoadingPaymentIntegration(true)
+            setIsSelect(true)
+            // setLoadingPaymentIntegration(false)
+
+            /* setPaymentTarget(
                 useSalesStore.getState().listSalesActives,
                 useSalesStore.getState().saleIdActive,
-                id)
+                id
+            ) */
         } else if (voucherTarget) {
             setVoucherTarget(
                 useSalesStore.getState().listSalesActives,
@@ -27,10 +37,15 @@ export default function PaymentButton ({ key, id, icon, title, paymentTarget, se
                     {icon}
                     <p className="dark:text-white/60 text-black uppercase  text-xl font-bold ">{title}</p>
                 </Button>
-                : <Button size="lg" className="flex flex-col border border-primary-200 dark:border-secondary-200  w-44 h-44  dark:bg-secondary-500 bg-primary-50" isIconOnly variant="shadow" aria-label=""
+                : <Button
+                    isLoading = {loadingPaymentIntegration}
+                    isIconOnly ={!loadingPaymentIntegration}
+                    size="lg"
+                    className="flex flex-col border border-primary-200 dark:border-secondary-200  w-44 h-44  dark:bg-secondary-500 bg-primary-50"
+                    variant="shadow"
                     onClick={handleButton}>
-                    {icon}
-                    <p className="dark:text-white/60 text-black uppercase  text-xl font-bold ">{title}</p>
+                    {loadingPaymentIntegration ? <></> : icon}
+                    <p className={`dark:text-white/60 text-black uppercase  ${!isSelect ? 'text-xl' : 'text-sm'} font-bold`}>{loadingPaymentIntegration ? 'Esperando pago' : title}</p>
                 </Button>}
         </div>
     )
