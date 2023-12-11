dc2dc836a299c3771c671c32dccb370279c95544
Dionisio Olivares <dionisio.olivares23@gmail.com>
2023-12-11 11:57:13 -0300

    fix window error

--- app/(layout-app)/reports/components/MainReportView.js
+++ app/(layout-app)/reports/components/MainReportView.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { Card, CardBody, CardHeader, CardFooter, Image, Button } from '@nextui-org/react'
 import InfoCard from '@/components/ui/infoCard'
 import TableSales from '@/components/ui/TableSales'
-import PieChart from '@/components/ui/pieChart'
+// import PieChart from '@/components/ui/pieChart'
 // import AreaChart from '@/components/ui/areaChart'
 // import Filter from './Filter/Filter'
 import useReportsStore from './store'
@@ -179,7 +179,7 @@ const ReportView = () => {
                                         </div>
 
                                         <div className='col-span-2 w-full h-full'>
-                                            <PieChart data = {dataModelPieChart} isLoading={dataModelPieChartLoading} />                                        </div>
+                                            {/* <PieChart data = {dataModelPieChart} isLoading={dataModelPieChartLoading} /> */}                                        </div>
                                     </section>
                                     <section className="col-span-2 mt-3 md:mt-0">
                                         {/* <AreaChart data = {dataModelSalesTypes} isLoading={dataModelSalesTypesLoading} /> */}                                    </section>
