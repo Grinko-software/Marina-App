6bdfebe7f3ab6029103bf0d63eef391bc60b04af
Dionisio Olivares <dionisio.olivares23@gmail.com>
2023-12-11 13:44:32 -0300

    fix window error

--- components/ui/pieChart.js
+++ components/ui/pieChart.js
@@ -1,8 +1,9 @@
-/* eslint-disable no-unused-vars */
 'use client'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Card, CardBody, CardHeader, Skeleton } from '@nextui-org/react'
 import dynamic from 'next/dynamic'
+
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
 
 const PieChart = ({ data, isLoading }) => {
     return (
@@ -16,21 +17,6 @@ const PieChart = ({ data, isLoading }) => {
     )
 }
 const WidgetReport = ({ children, className, title, isLoading, data }) => {
-    const [ChartComponent, setChartComponent] = useState(null)
-
-    async function InitChart () {
-        if (typeof window !== 'undefined') {
-            import('react-apexcharts').then(({ default: Chart }) => {
-                const chart = new Chart()
-                setChartComponent(chart)
-            })
-        }
-    }
-
-    useEffect(() => {
-        InitChart()
-    }, [data])
-
     return <Card className={'w-auto flex-1 transition duration-1000 ease-in-out text-opacity-50 hover:text-opacity-100 dark:bg-secondary-400 bg-primary-50/80 hover:bg-primary-50 transform hover:scale-[1.01] text-black ' + className}>
         <CardHeader >
             {isLoading
@@ -38,10 +24,10 @@ const WidgetReport = ({ children, className, title, isLoading, data }) => {
                 : <h4 className="text-primary-500 dark:text-white font-semibold text-xl">{title}</h4>}
         </CardHeader>
         <CardBody>
-            {isLoading || !ChartComponent
+            {isLoading
                 ? <Skeleton className="h-[20rem] w-[20rem] rounded-md"></Skeleton>
                 : <div id="chart">
-                    <ChartComponent
+                    <Chart
                         options={data?.options}
                         series={data?.series}
                         type="pie"
