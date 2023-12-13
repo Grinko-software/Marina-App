a1f185e076796d27fa5f4dbb9401b1905c67e799
Dionisio Olivares <dionisio.olivares23@gmail.com>
2023-12-13 13:44:00 -0300

    a

--- stores/user.js
+++ stores/user.js
@@ -41,7 +41,7 @@ const useAuthStore = create(
                             if (userType === 'admin') set({ isAdmin: true })
                         } else {
                             // set({ error: statusCode + ' ' + (error || message || statusText) })
-                            set({ error: 'Error' })
+                            set({ error: true })
                         }
                         set({ loading: false })
                     })
