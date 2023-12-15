c936410c3b65c4a819c6b9017a87a6928a7a2eca
sebastian <ssp013sanchez@gmail.com>
2023-12-15 17:23:18 -0300

    added cancel button

--- app/(layout-app)/sales/components/loadingSale.js
+++ app/(layout-app)/sales/components/loadingSale.js
@@ -6,9 +6,21 @@ import Lottie from 'lottie-react'
 import useInvoiceStore from './invoice/store'
 import useMachineStore from './store/machine'
 import cardloading from '@/assets/gifs/card-loading.json'
+import useSalesStore from '../store'
 export default function LoadingSale ({ payment, loadingSale, setPageTarget, setPayment, isOpen, onClose, setGoPay, totalPay, payDetailed, setPayDetailed, listSales, createSale, paymentTarget, voucherTarget, clearList, pageTarget, onOpen, setPaymentTarget, setSearchInput, setVoucherTargetValue }) {
     const { status, setStatus } = useMachineStore(({ status, setStatus }) => ({ status, setStatus }))
     const { targetCustomer, setTargetCustomer } = useInvoiceStore(({ targetCustomer, setTargetCustomer }) => ({ targetCustomer, setTargetCustomer }))
+    const {
+        listSalesActives,
+        saleIdActive,
+        removeSale
+    } = useSalesStore()
+    const handleCancelSale = () => {
+        setPaymentTarget(listSalesActives, saleIdActive, null)
+        setPayDetailed(null)
+        onClose()
+        setGoPay(false)
+    }
     useEffect(() => {
         if (status) {
             onOpen()
@@ -31,7 +43,18 @@ export default function LoadingSale ({ payment, loadingSale, setPageTarget, setP
                     <section className="h-[10rem] w-[15rem] rounded-full border border-primary-200 bg-primary-100 ">
                         <Lottie animationData={cardloading} loop={true} />
                     </section>
-                    <Button isLoading={true} className='text-2xl h-[5rem] bg-green-600 text-white'>{status}</Button>
+                    <section className="flex flex-col items-center h-[11rem] space-y-3">
+                        <Button isLoading={true} className='text-2xl w-[15rem] h-[5rem] bg-green-600 text-white'>{'pendiente'}</Button>
+                        <Button
+                            color="danger"
+                            variant="shadow"
+                            className="text-2xl h-[5rem] w-[15rem] "
+                            onClick={handleCancelSale}
+                        >
+                            Cancelar
+                        </Button>
+                    </section>
+
                 </ModalBody>
 
             </ModalContent>
