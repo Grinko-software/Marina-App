ac87981788ae900e175b8974180504609f381e09
sebastian <ssp013sanchez@gmail.com>
2024-01-23 11:33:43 -0300

    printer

---
+++ app/home/test-printer.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+const ThermalPrinter = require('node-thermal-printer').printer
+const PrinterTypes = require('node-thermal-printer').types
+
+const printer = new ThermalPrinter({
+    type: PrinterTypes.EPSON,
+    interface: 'tcp://192.168.123.100:9100'
+})
+
+printer.alignCenter()
+printer.println('Hello world')
+printer.cut()
+
+try {
+    const execute = printer.execute()
+    console.log('Print done!')
+    if (execute) {
+        console.log('Print')
+    } else {
+        console.log('Not print')
+    }
+} catch (error) {
+    console.error('Print failed:', error)
+}
