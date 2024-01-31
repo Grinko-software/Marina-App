1919a93dcaca8cc151f45d0c9593ae2b191ef917
sebastian <ssp013sanchez@gmail.com>
2024-01-30 22:53:27 -0300

    control w img barcode

--- components/barcode.js
+++ components/barcode.js
@@ -10,23 +10,23 @@ export default function Bacode (props) {
         }
     })
     return (
-
-        <div ref={refBarcode} className='flex flex-col items-center w-full bg-white'>
-            {showDetail
-                ? (
-                    <div>
-                        <div className="text-black text-2xl w-auto font-bold">{productName}</div>
-                        <div className=" flex flex-col items-center ">
-                            <div className='flex flex-row'>
-                                <div className="text-black text-2xl">{'$'}</div>
-                                <div className=" text-black text-2xl">{productCost}</div>
+        <section className='flex flex-col items-center'>
+            <div ref={refBarcode} className='w-[20rem] flex flex-col items-center bg-white'>
+                {showDetail
+                    ? (
+                        <div>
+                            <div className="text-black text-2xl w-auto font-bold">{productName}</div>
+                            <div className=" flex flex-col items-center ">
+                                <div className='flex flex-row'>
+                                    <div className="text-black text-2xl">{'$'}</div>
+                                    <div className=" text-black text-2xl">{productCost}</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-                : null}
-            <div className="flex "><svg ref={inputRef} /></div>
-        </div>
-
+                    )
+                    : null}
+                <div className="flex "><svg ref={inputRef} /></div>
+            </div>
+        </section>
     )
 }
