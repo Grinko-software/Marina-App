b6b1eeb5ffb82dbde78fb1391ed9c24df26b8084
Dionisio Olivares <dionisio.olivares23@gmail.com>
2024-10-29 18:36:41 -0300

    a

--- app/(layout-app)/modules/[module]/components/WorkerPerformance/components/Card.js
+++ app/(layout-app)/modules/[module]/components/WorkerPerformance/components/Card.js
@@ -15,19 +15,10 @@ export default function CardTask ({
     return (
         <div onClick={openDetail}>
             <Card className="py-2">
-                <CardHeader className="pb-0 pt-1 px-4 flex-col items-start">
-                    <div className="flex flex-row gap-x-2 justify-center items-center relative">
-                        <Avatar isBordered radius="sm" src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
-                        <div className="flex flex-col items-start">
-                            <p className="text-tiny uppercase font-bold">{user || '-'}</p>
-                            <small className="text-default-500">{'Empleado'}</small>
-                        </div>
-                    </div>
-                </CardHeader>
                 <CardBody>
                     <h4 className="font-bold text-large uppercase">{title}</h4>
                 </CardBody>
-                <CardFooter className="overflow-visible py-2">
+                <CardFooter className="overflow-visible pt-0">
                     <div className="flex flex-col items-start">
                         <p className="text-tiny uppercase font-bold">{description}</p>
                         <small className="text-default-500">Fecha límite: {getMoment(dateLimit).calendar() || '-'}</small>
