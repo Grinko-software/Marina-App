452ac1d737b39334341b634f80116d0e7fabe8a5
grinko.software@gmail.com <grinko.software@gmail.com>
2024-11-25 16:30:21 -0300

    delete maximun

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout ({ children }) {
             <head>
                 <meta
                     name="viewport"
-                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+                    content="width=device-width, initial-scale=1.0, user-scalable=yes"
                 />
                 <title>Marina Market App</title>
             </head>
