23cabb4325000e047f5dc5a476ec7df7d6d2bed9
Nicolás <n.garciasantelices@gmail.com>
2025-03-10 16:23:35 -0300

    chore: Update loading state in reports components

--- app/(layout-app)/reports/components/Filter/store.js
+++ app/(layout-app)/reports/components/Filter/store.js
@@ -1,15 +1,32 @@
 import { create } from 'zustand'
 
+const today = new Date()
+today.setHours(0, 0, 0, 0)
+
+const oneWeekAgo = new Date(today)
+oneWeekAgo.setDate(today.getDate() - 7)
+
 const useFilterStore = create((set) => ({
-    fromDate: undefined,
-    toDate: undefined,
-    rangeType: undefined,
-    periodQuantity: undefined,
-    error: null,
-    loading: false,
-    setRangeType: (value) => set({ rangeType: value }),
-    setFromDate: (value) => set({ fromDate: value }),
-    setToDate: () => (value) => set({ toDate: value }),
+    fromDate: oneWeekAgo,
+    toDate: today,
+    periodQuantity: Math.ceil((today.getTime() - oneWeekAgo.getTime()) / (1000 * 3600 * 24)),
+    periodRange: 'Day',
+    setFromDate: (value) => set((state) => {
+        const newFromDate = new Date(value)
+        newFromDate.setHours(0, 0, 0, 0)
+        return {
+            fromDate: newFromDate,
+            periodQuantity: Math.ceil((state.toDate.getTime() - newFromDate.getTime()) / (1000 * 3600 * 24))
+        }
+    }),
+    setToDate: (value) => set((state) => {
+        const newToDate = new Date(value)
+        newToDate.setHours(0, 0, 0, 0)
+        return {
+            toDate: newToDate,
+            periodQuantity: Math.ceil((newToDate.getTime() - state.fromDate.getTime()) / (1000 * 3600 * 24))
+        }
+    }),
     setPeriodQuantity: (value) => set({ periodQuantity: value })
 }))
 
