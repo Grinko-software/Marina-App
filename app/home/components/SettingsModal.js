adfa36ca9661657e6e8930cac819d4b2b1e93848
grinko.software@gmail.com <grinko.software@gmail.com>
2025-03-13 13:30:27 -0300

    fix: Update Checkbox component usage in SettingsModal for better event handling

--- app/home/components/SettingsModal.js
+++ app/home/components/SettingsModal.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
 'use client'
 import React, { useState, useEffect } from 'react'
-import { Modal, Checkbox, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Divider } from '@nextui-org/react'
+import { Checkbox } from '@nextui-org/checkbox'
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Divider } from '@nextui-org/react'
 import useSettingsStore, { DEFAULT_SELECTED } from './../../../stores/settings'
 
 export default function SettingModal ({ isOpen, onClose }) {
@@ -74,7 +75,9 @@ export default function SettingModal ({ isOpen, onClose }) {
                                         <Checkbox
                                             key={index}
                                             isSelected={isSelected}
-                                            onClick={() => { setSelectedPostMachine(item) }}
+                                            onChange={() => {
+                                                setSelectedPostMachine(item)
+                                            }}
                                             color="danger">
                                             {item?.serial_number || item?.label}
                                         </Checkbox>
@@ -93,7 +96,7 @@ export default function SettingModal ({ isOpen, onClose }) {
                                             <Checkbox
                                                 key={index2}
                                                 isSelected={isCashRegistrySelected}
-                                                onClick={() => { setSelectedCashRegister(cashRegister) }}
+                                                onChange={() => { setSelectedCashRegister(cashRegister) }}
                                                 color="danger">
                                                 {cashRegister?.name || cashRegister?.label}
                                             </Checkbox>
